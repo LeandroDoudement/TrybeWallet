90dd277f94c3d534596851cd307d8ce359c6d7ea
Leandro França <leandrodoudement@gmail.com>
2022-10-31 19:17:22 -0300

    Inicio de projeto e Requisito 1 | Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,80 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { setEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleClick = () => {
+    const { email } = this.state;
+    const { dispatch, history } = this.props;
+    dispatch(setEmail(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const passwordMinimum = 6;
+    const submitVerification = (emailRegex.test(email)
+    && password.length >= passwordMinimum);
+    return (
+      <>
+        <label htmlFor="emailInput">
+          Digite seu email:
+          <input
+            type="email"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+            name="email"
+          />
+        </label>
+        <label htmlFor="passwordInput">
+          Digite sua senha:
+          <input
+            type="password"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+            name="password"
+          />
+        </label>
+        <button
+          type="button"
+          disabled={ !submitVerification }
+          onClick={ this.handleClick }
+          data-testid="login-submit-button"
+        >
+          Entrar
+        </button>
+      </>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  globalEmail: state.email,
+  globalPassword: state.password,
+});
+
+export default connect(mapStateToProps)(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,8 @@
-// Coloque aqui suas actions
+export const SET_EMAIL = 'SET_EMAIL';
+
+export const setEmail = (value) => ({
+  type: SET_EMAIL,
+  payload: {
+    email: value,
+  },
+});
