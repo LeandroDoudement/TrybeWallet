d8def93cf7bdf427f078b28f7087d5413612d4a9
Leandro França <leandrodoudement@gmail.com>
2022-11-01 00:50:23 -0300

    Requisito 2 | Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import store from '../redux/store';
 
 class Header extends Component {
   render() {
+    const state = store.getState();
+    const { email } = state.user;
     return (
-      <div>Header</div>
+      <header>
+        <p data-testid="email-field">{email}</p>
+        <p data-testid="total-field">Despesa total: 0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
     );
   }
 }
